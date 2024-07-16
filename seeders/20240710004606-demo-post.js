398399041fea6a90469184af78c1a6ba0de94231
Eric Barreto <ericbarreto521@gmail.com>
2024-07-16 17:35:35 -0300

    fix: seed

--- seeders/20240710004606-demo-post.js
+++ seeders/20240710004606-demo-post.js
@@ -1,26 +1,26 @@
-'use strict';
-const { faker } = require('@faker-js/faker');
+"use strict";
+const { faker } = require("@faker-js/faker");
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    const users = await queryInterface.sequelize.query(
-      'SELECT id from Users;',
-    );
+	up: async (queryInterface, Sequelize) => {
+		const users = await queryInterface.sequelize.query(
+			"SELECT id from Users;"
+		);
 
-    const userRows = users[0];
+		const userRows = users[0];
 
-    const posts = Array.from({ length: 14 }).map(() => ({
-      title: faker.lorem.sentence(6),
-      content: faker.lorem.paragraphs(3),
-      user_id: userRows[Math.floor(Math.random() * userRows.length)].id,
-      created_at: new Date(),
-      updated_at: new Date(),
-    }));
+		const posts = Array.from({ length: 15 }).map(() => ({
+			title: faker.lorem.sentence(6),
+			content: faker.lorem.paragraphs(30),
+			user_id: userRows[Math.floor(Math.random() * userRows.length)].id,
+			created_at: new Date(),
+			updated_at: new Date(),
+		}));
 
-    await queryInterface.bulkInsert('posts', posts, {});
-  },
+		await queryInterface.bulkInsert("posts", posts, {});
+	},
 
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('posts', null, {});
-  }
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.bulkDelete("posts", null, {});
+	},
 };
